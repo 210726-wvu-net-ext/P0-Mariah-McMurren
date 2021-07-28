1100d1eaea60c4060e5c0c61fa3434aa115e978f
Sean Fitzmaurice <seanfitzmaurice27@gmail.com>
2021-07-28 13:47:52 -0400

    added comment toHello.sh

--- Hello.sh
+++ Hello.sh
@@ -4,4 +4,4 @@ echo 'What are your 3 favorite movies?'
 read movie1 movie2 movie3
 echo "Your 3 favorite movies are $movie1, $movie2, and $movie3"
 
-#adding a comment!
+#I see you used 3 variables!
